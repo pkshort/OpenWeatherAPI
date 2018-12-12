6a57558678da3432f9690ac9253b3a8637981cdc
pkshort <pshort@ithaca.edu>
2018-12-12 02:43:23 -0500

    File updates

--- src/weather.py
+++ src/weather.py
@@ -1,49 +1,59 @@
+# Created by P. Kevin Short II
 
+#
 # load in csv file
 # call weather API for locations
 # output csv: location, temperature, wind speed, weather description
+#
 
 import csv, requests, pprint, json, time
 
 with open('locations.csv') as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=',')
 
-    #api request
-    api_key = "88aa68dfe44643228b20a74fbd7a47c9"
+    # api request
+    # enter your API key in the variable api_key
+    api_key = ""
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    # imperial units switches the temperature from Kelvin to Farenheit
     units = "imperial"
 
-    #setting up output header
+    # setting up output header
     loc_str = "Location"
     temp_str = "Current Temperature"
     wind_str = "Wind Speed"
     desc_str = "Description"
 
-    #built output header
+    # built output header
     output_header = [loc_str, temp_str, wind_str, desc_str]
 
-    #output file - output.csv
+    # output file - output.csv
     output_csv = "output.csv"
 
-    #opening output file
+    # opening output file
     with open(output_csv, mode = "w") as output_file:
         output_writer = csv.writer(output_file, delimiter=',')
         output_writer.writerow(output_header)
         for row in csv_reader:
             for key, value in row.items():
 
-                    #can call API 60 times per second
+                    # can call API 60 times per second
+                    # sleep ensures that the API is called under 60 times
                     time.sleep(.17)
                     city_name = str(value);
-                    #built url
+                    # built url
                     complete_url = (base_url + "appid=" + api_key +
                             "&q=" + city_name +
                             "&units=" + units)
 
                     response = requests.get(complete_url)
                     my_json = response.json()
-                    #if statement to make sure the API call was successfull
+
+                    # if statement to make sure the API call was successfull
                     if my_json["cod"] != "404":
+
+                        # retrieving the data from the json response
                         rec = my_json["main"]
                         current_temp = rec["temp"]
                         wind = my_json["wind"]
@@ -51,12 +61,12 @@ with open('locations.csv') as csv_file:
                         weather = my_json["weather"]
                         weather_desc = weather[0]["description"]
 
-                        #output_writer writes to csv file
+                        # output_writer writes to csv file
                         output_writer.writerow([str(city_name),
                                 str(current_temp), str(wind_speed),
                                 str(weather_desc)])
 
-
+                        # prints the output to the console after writing
                         print("\nCity: " + str(city_name) +
                                 "\n\tTemperature: " + str(current_temp) +
                                 "\n\tWind Speed: " + str(wind_speed) +
@@ -66,4 +76,4 @@ with open('locations.csv') as csv_file:
                     else:
                         print("City not found..")
 
-    #location, temperature, wind speed, description
+    # location, temperature, wind speed, description
